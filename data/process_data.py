f80a4229d9cae2fc9df96b4f090901e0e1756718
monjaburkard <monja.burkard@gmail.com>
2021-08-25 08:58:00 +0200

    add ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,32 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath,encoding='utf-8')
+    categories = pd.read_csv(categories_filepath, encoding='utf-8')
+    df = pd.merge(messages, categories, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
-
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories[:1]
+    category_colnames = np.array(row.apply(lambda x: x.str[0:-2])).flatten()
+    categories.columns = category_colnames
+    for column in categories:
+        categories[column] = categories[column].astype(str).str[-1]
+        categories[column] = categories[column].astype(int)
+    categories = categories.replace(2, 1)
+    df =df.drop(['categories'], axis=1)
+    df = pd.concat([df, categories], axis=1)
+    df = df.drop_duplicates()
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('DisasterResponse', engine, index=False, if_exists='replace')
 
 
 def main():
@@ -20,7 +36,7 @@ def main():
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+        df = load_data('./disaster_messages.csv', './disaster_categories.csv')
 
         print('Cleaning data...')
         df = clean_data(df)
